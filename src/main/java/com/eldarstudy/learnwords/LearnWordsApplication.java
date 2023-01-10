2f5e0216c8747fd930c86e35b4e157c58f77e3d5
eldar <e9991340203@yandex.ru>
2023-01-10 11:20:14 +0500

    initial commit

---
+++ src/main/java/com/eldarstudy/learnwords/LearnWordsApplication.java
@@ -0,0 +1,13 @@
+package com.eldarstudy.learnwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldarstudy/learnwords/LearnWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldarstudy.learnwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
