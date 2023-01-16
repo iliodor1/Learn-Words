0ec7425ef40d985b7515f8599c4665ac6accc40e
eldar <e9991340203@yandex.ru>
2023-01-16 10:55:28 +0500

    feat: add CardRepository

--- src/main/java/com/eldarstudy/learnwords/models/Card.java
+++ src/main/java/com/eldarstudy/learnwords/models/Card.java
@@ -1,9 +1,20 @@
 package com.eldarstudy.learnwords.models;
 
-import lombok.Data;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String frontSide;
     private String backSide;
--- src/main/java/com/eldarstudy/learnwords/repositories/CardRepository.java
+++ src/main/java/com/eldarstudy/learnwords/repositories/CardRepository.java
@@ -1,6 +1,8 @@
 package com.eldarstudy.learnwords.repositories;
 
 import com.eldarstudy.learnwords.models.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository{
+public interface CardRepository extends JpaRepository<Card, Long> {
+
 }
